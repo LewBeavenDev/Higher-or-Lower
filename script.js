f8a67e65dc7cdcee33f42a6b18f0b80ed313c61d
LewBeavenDev <elu95@hotmail.co.uk>
2024-03-14 22:23:40 +0000

    Now auto adds alt tag to card that is dealt

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-
 // Creating Deck of Cards
 let deck = [];
-let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10","jack","queen","king","ace"];
+let ranks = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "jack",
+  "queen",
+  "king",
+  "ace",
+];
 let suits = ["spades", "clubs", "hearts", "diamonds"];
 let currentCardValue = 0;
 
 // Function loops through 'ranks' and 'suits' array and populates a new array (deck).
 function createDeck() {
-    
   for (let suit in suits) {
     for (let rank in ranks) {
       deck.push(`${ranks[rank]}_of_${suits[suit]}`);
     }
-  }  
+  }
   return deck;
 }
 
@@ -20,26 +32,26 @@ createDeck();
 
 console.log(deck);
 
-
 // Shuffle function
 // For every card in the deck, selects a random card, and replaces that with the new card. Resulting in a truly shuffled deck of cards.
 
-
 function shuffleDeck() {
-    for (let card in deck) {
-        let tempCard = deck[card];
-        let randomCard = Math.floor(Math.random() * 52);
-        deck[card] = deck[randomCard];
-        deck[randomCard] = tempCard;
-    }
+  for (let card in deck) {
+    let tempCard = deck[card];
+    let randomCard = Math.floor(Math.random() * 52);
+    deck[card] = deck[randomCard];
+    deck[randomCard] = tempCard;
+  }
 }
 
 shuffleDeck();
 
-console.log(deck);
+// console.log(deck);
 
 let card = deck.pop();
 console.log(card);
 
-document.querySelector(".currentCard").src=`/assets/PNG-cards-1.3/${card}.png`
-
+document.querySelector(
+  ".currentCard"
+).src = `./assets/PNG-cards-1.3/${card}.png`;
+document.querySelector(".currentCard").alt = `${card}`;
